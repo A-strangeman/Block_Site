ac3ac5ca5497421c43ed6d9b417bd372bdc4171a
Roshan Gupta <roshankumar.22211261@viit.ac.in>
2026-05-22 11:04:18 +0530

    chore(extension): add ESLint config, install deps, autofix

---
+++ extension/.eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  env: { es2020: true },
+  rules: {},
+};
