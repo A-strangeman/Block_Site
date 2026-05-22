cfab83c18d995a43bcfc4e29ac6a8f0f143487ce
Roshan Gupta <roshankumar.22211261@viit.ac.in>
2026-05-22 11:09:28 +0530

    ci: add extension ESLint job; tighten ESLint rules for extension

--- extension/.eslintrc.cjs
+++ extension/.eslintrc.cjs
@@ -2,10 +2,16 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
+    ecmaVersion: 2022,
     sourceType: 'module',
     project: ['./tsconfig.json'],
   },
-  env: { es2020: true },
-  rules: {},
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  env: { es2022: true },
+  rules: {
+    // Project-level overrides can go here. Keep strict defaults for now.
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
 };
